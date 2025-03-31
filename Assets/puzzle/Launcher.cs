921844523f64531f0be096c554fb76ffcc674564
junhek2525 <135084879+junhek2525@users.noreply.github.com>
2025-03-31 21:14:15 +0900

    e
    e

--- Assets/puzzle/Launcher.cs
+++ Assets/puzzle/Launcher.cs
@@ -10,6 +10,9 @@ public class Launcher : MonoBehaviour
 
     public GameObject objectToSpawn;  // 소환할 오브젝트를 연결할 변수
     public Transform spawnPoint;      // 소환될 위치 (Transform을 통해 지정)
+    public enum Direction { Up, Down, Left, Right }
+    public Direction currentDirection;
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +32,22 @@ public class Launcher : MonoBehaviour
     //}
     public void shot()
     {
+        switch (currentDirection)
+        {
+            case Direction.Up:
+                transform.rotation = Quaternion.Euler(0, 0, 0); // 위쪽
+                break;
+            case Direction.Down:
+                transform.rotation = Quaternion.Euler(0, 0, 180); // 아래쪽
+                break;
+            case Direction.Left:
+                transform.rotation = Quaternion.Euler(0, 0, 90); // 왼쪽
+                break;
+            case Direction.Right:
+                transform.rotation = Quaternion.Euler(0, 0, -90); // 오른쪽
+                break;
+        }
+
         if(curtime <=0f)
         {
             Instantiate(objectToSpawn, spawnPoint.position, spawnPoint.rotation);
@@ -41,4 +60,12 @@ public class Launcher : MonoBehaviour
         
 
     }
+
+
+    
+
+    //void Update()
+    //{
+        
+    //}
 }
